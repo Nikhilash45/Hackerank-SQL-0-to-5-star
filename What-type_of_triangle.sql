cbcd951fee795c8a18e1e17cf29944cffcafdd05
Nikhil <102281173+Nikhilash45@users.noreply.github.com>
2024-07-21 19:02:52 +0530

    Delete What-type_of_triangle.sql

--- What-type_of_triangle.sql
+++
@@ -1,8 +0,0 @@
-select 
-Case
-when a+b<=c or b+c<=a or c+a<=b then "Not A Triangle" 
-When a =b And b=c and a=c then "Equilateral"
-when a=b or b=c or a=c then "Isosceles"
-when a<>b and b<>c and a<>c then "Scalene"
-end 
- from TRIANGLES;
