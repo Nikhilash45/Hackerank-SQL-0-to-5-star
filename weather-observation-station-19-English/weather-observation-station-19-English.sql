bc0b2fdd95946ca4b64ff5d5eae5662e3109e756
Nikhil <102281173+Nikhilash45@users.noreply.github.com>
2024-08-09 23:47:53 +0530

    Delete weather-observation-station-19-English directory

--- weather-observation-station-19-English/weather-observation-station-19-English.sql
+++
@@ -1,3 +0,0 @@
-WITH Nikk AS  (Select POWER(min(LAT_N)-Max(LAT_N),2) as P_1  , POWER(min(LONG_W)-Max(LONG_W),2) as P_2 from STATION) ,
-sann as (Select SQRT(P_1+P_2) as map from Nikk)
-select Round(map,4) from sann
